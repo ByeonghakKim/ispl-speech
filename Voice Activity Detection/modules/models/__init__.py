69af54e3b5f4334bcb34aa40639b9126653a2d42
YoungloLee <lyrzzang@gmail.com>
2020-04-20 18:38:10 +0900

    Revert "Voice Activity Detection"
    This reverts commit d9e6c62e6677461ab93329d3c88a791ae9afe32d.

--- Voice Activity Detection/modules/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .proposed import Proposed
-
-
-def create_model(name, hparams):
-    if name == 'Proposed':
-        return Proposed(hparams)
-    else:
-        raise Exception('Unknown model: ' + name)
